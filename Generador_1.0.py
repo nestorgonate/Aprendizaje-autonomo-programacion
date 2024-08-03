2b87ba0f9cf4680f41ffe0a8557d586ba1c1102b
nestorgonate <negavilaneson@uide.edu.ec>
2024-08-02 21:51:02 -0500

    Generador_1.0.py
    Se agrega funcion para que el condicional no distinga entre mayusculas y minusculas.

--- Generador_1.0.py
+++ Generador_1.0.py
@@ -1,14 +1,14 @@
 import random #Libreria para permitir elegir aleatoriamente
 import string #Libreria para tener acceso a simbolos, letras y numeros
-pregunta = input("Quieres generar una contraseña?: ")
+pregunta = input("Quieres generar una contraseña?: ").lower()
 def generador():
     contenido_contrasena = string.ascii_letters + string.punctuation + string.digits #Usando la libreria string accede a letras, simbolos y numeros para juntarlos
     contrasena = random.choices(contenido_contrasena, k=12) #Usando la libreria random elige de la variable contenido_contrasena contenido aleatorio, la k determina longitud
     contrasena = "".join(contrasena) #.join() convierte contrasena de lista a string
     print(contrasena)
-if pregunta.lower == "Si":
+if pregunta == "si":
     generador()
-elif pregunta.lower == "No":
+elif pregunta == "no":
     print("Hasta luego")
 else:
     print("Ingresa una respuesta valida")
